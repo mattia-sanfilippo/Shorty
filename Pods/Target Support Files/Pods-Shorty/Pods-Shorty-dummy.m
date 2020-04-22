611c003f35d04c07fa064cb9dffac487219ca2cf
Mattia Sanfilippo <imattolo@me.com>
2020-04-22 09:10:54 +0200

    Added Comets to the Main View. Amazing!

---
+++ Pods/Target Support Files/Pods-Shorty/Pods-Shorty-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Shorty : NSObject
+@end
+@implementation PodsDummy_Pods_Shorty
+@end
--- Shorty/Controller/ShortyViewController.swift
+++ Shorty/Controller/ShortyViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Comets
 
 class ShortyViewController: UIViewController {
     
@@ -16,6 +17,46 @@ class ShortyViewController: UIViewController {
     
     @IBOutlet weak var urlField: UITextField!
     
+    fileprivate func showComets() {
+        // Customize your comet
+        let width = view.bounds.width
+        let height = view.bounds.height
+        let comets = [Comet(startPoint: CGPoint(x: 100, y: 0),
+                            endPoint: CGPoint(x: 0, y: 100),
+                            lineColor: UIColor.systemTeal.withAlphaComponent(0.2),
+                            cometColor: UIColor.systemTeal),
+                      Comet(startPoint: CGPoint(x: 0.4 * width, y: 0),
+                            endPoint: CGPoint(x: width, y: 0.8 * width),
+                            lineColor: UIColor.systemTeal.withAlphaComponent(0.2),
+                            cometColor: UIColor.systemTeal),
+                      Comet(startPoint: CGPoint(x: 0.8 * width, y: 0),
+                            endPoint: CGPoint(x: width, y: 0.2 * width),
+                            lineColor: UIColor.systemTeal.withAlphaComponent(0.2),
+                            cometColor: UIColor.systemTeal),
+                      Comet(startPoint: CGPoint(x: width, y: 0.2 * height),
+                            endPoint: CGPoint(x: 0, y: 0.25 * height),
+                            lineColor: UIColor.systemTeal.withAlphaComponent(0.2),
+                            cometColor: UIColor.systemTeal),
+                      Comet(startPoint: CGPoint(x: 0, y: height - 0.8 * width),
+                            endPoint: CGPoint(x: 0.6 * width, y: height),
+                            lineColor: UIColor.systemTeal.withAlphaComponent(0.2),
+                            cometColor: UIColor.systemTeal),
+                      Comet(startPoint: CGPoint(x: width - 100, y: height),
+                            endPoint: CGPoint(x: width, y: height - 100),
+                            lineColor: UIColor.systemTeal.withAlphaComponent(0.2),
+                            cometColor: UIColor.systemTeal),
+                      Comet(startPoint: CGPoint(x: 0, y: 0.8 * height),
+                            endPoint: CGPoint(x: width, y: 0.75 * height),
+                            lineColor: UIColor.systemTeal.withAlphaComponent(0.2),
+                            cometColor: UIColor.systemTeal)]
+        
+        // draw line track and animate
+        for comet in comets {
+            view.layer.addSublayer(comet.drawLine())
+            view.layer.addSublayer(comet.animate())
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
@@ -23,6 +64,8 @@ class ShortyViewController: UIViewController {
         
         // MARK: Hide keyboard on tap outside the URL Text Field
         view.addGestureRecognizer(UITapGestureRecognizer(target: view, action: #selector(UIView.endEditing)))
+        
+        showComets()
     }
 
     @IBAction func makeItShorty(_ sender: Any) {
@@ -34,6 +77,8 @@ class ShortyViewController: UIViewController {
             
         }
     }
+    
+    
     
     
 }
